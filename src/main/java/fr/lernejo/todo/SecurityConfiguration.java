ee9e6e4aadbf326672891705b92dee25c3432128
ibluestarz <105120484+ibluestarz@users.noreply.github.com>
2024-12-12 19:06:45 +0100

    Update SecurityConfiguration.java

--- src/main/java/fr/lernejo/todo/SecurityConfiguration.java
+++ src/main/java/fr/lernejo/todo/SecurityConfiguration.java
@@ -1,4 +1,64 @@
 package fr.lernejo.todo;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.NoOpAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.Map;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(eir -> eir
+                .requestMatchers(HttpMethod.POST, "/api/account").permitAll()
+                .anyRequest().authenticated()
+            )
+            .httpBasic(Customizer.withDefaults())
+            .exceptionHandling(e -> e.defaultAuthenticationEntryPointFor(
+                new NoOpAuthenticationEntryPoint(),
+                new AntPathRequestMatcher("/api/account", HttpMethod.POST.name()))
+            )
+        ;
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new DelegatingPasswordEncoder(
+            "argon2",
+            Map.of("argon2", Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8()));
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authProvider(PasswordEncoder passwordEncoder, UserDetailsService userDetailsService) {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder);
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http, DaoAuthenticationProvider authProvider) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+            .authenticationProvider(authProvider)
+            .build();
+    }
 }
