f9dcca6e5eef66e90c13d06c24bb090cfd9f067f
ibluestarz <105120484+ibluestarz@users.noreply.github.com>
2024-12-12 19:07:17 +0100

    Update UserController.java

--- src/main/java/fr/lernejo/todo/UserController.java
+++ src/main/java/fr/lernejo/todo/UserController.java
@@ -1,4 +1,43 @@
 package fr.lernejo.todo;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/account")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(@RequestBody NewUserDto user) throws Exception {
+        System.out.println("New create user request");
+        if (user.getEmail() == null || user.getPassword() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Email and password must be provided");
+        }
+        try {
+            final UserDto res = userService.createUser(user);
+            return ResponseEntity.created(null).body(res);
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
+        } catch (BadCreateUserRequestException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @GetMapping("/self")
+    public ResponseEntity<GetUserDto> getUser(Principal principal) throws Exception {
+        final GetUserDto res = userService.getUserByEmail(principal.getName());
+        return ResponseEntity.ok(res);
+    }
 }
