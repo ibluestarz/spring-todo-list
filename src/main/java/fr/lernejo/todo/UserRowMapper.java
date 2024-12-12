1968bc718d5cace5d90fbd378a6807dcad7ecbc0
ibluestarz <105120484+ibluestarz@users.noreply.github.com>
2024-12-12 19:08:29 +0100

    Update UserRowMapper.java

--- src/main/java/fr/lernejo/todo/UserRowMapper.java
+++ src/main/java/fr/lernejo/todo/UserRowMapper.java
@@ -1,4 +1,21 @@
 package fr.lernejo.todo;
 
-public class UserRowMapper {
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.UUID;
+
+public class UserRowMapper implements RowMapper<UserEntity> {
+    @Override
+    public UserEntity mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new UserEntity(
+            rs.getLong("id"),
+            UUID.fromString(rs.getString("uuid")),
+            rs.getTimestamp("created_at").toInstant(),
+            rs.getString("email"),
+            rs.getString("encoded_password")
+        );
+    }
+
 }
