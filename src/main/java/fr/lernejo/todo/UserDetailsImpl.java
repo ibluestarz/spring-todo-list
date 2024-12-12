7686cb25b618634135c45c6726129caa4a9bef3a
ibluestarz <105120484+ibluestarz@users.noreply.github.com>
2024-12-12 19:07:31 +0100

    Update UserDetailsImpl.java

--- src/main/java/fr/lernejo/todo/UserDetailsImpl.java
+++ src/main/java/fr/lernejo/todo/UserDetailsImpl.java
@@ -1,5 +1,38 @@
 package fr.lernejo.todo;
 
-public class UserDetailsImpl
-{
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Set;
+
+@Service
+public class UserDetailsImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    private final Map<String, User> usersMap = Map.of(
+        "mbappe", new User("mbappe", hash("mbappepwd"), Set.of()),
+        "barcola", new User("barcola", hash("barcolapwd"), Set.of())
+    );
+
+    public UserDetailsImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    private String hash(String password) {
+        return new SecurityConfiguration().passwordEncoder().encode(password);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new User(user.email(), user.password(), Set.of());
+    }
 }
