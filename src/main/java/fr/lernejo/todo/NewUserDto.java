49c43b70bb9930fc207b8acd4dfc428eb8743d0f
ibluestarz <105120484+ibluestarz@users.noreply.github.com>
2024-12-12 19:06:20 +0100

    Update NewUserDto.java

--- src/main/java/fr/lernejo/todo/NewUserDto.java
+++ src/main/java/fr/lernejo/todo/NewUserDto.java
@@ -1,4 +1,27 @@
 package fr.lernejo.todo;
 
 public class NewUserDto {
+    private String email;
+    private String password;
+
+    public NewUserDto(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
