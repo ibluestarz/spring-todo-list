afee6733ee7e2b5dbcf22f7c34df7562512a7642
ibluestarz <105120484+ibluestarz@users.noreply.github.com>
2024-12-12 19:08:04 +0100

    Update UserEntity.java

--- src/main/java/fr/lernejo/todo/UserEntity.java
+++ src/main/java/fr/lernejo/todo/UserEntity.java
@@ -1,4 +1,12 @@
 package fr.lernejo.todo;
 
-public class UserEntity {
+import java.time.Instant;
+import java.util.UUID;
+
+public record UserEntity(
+    Long id,
+    UUID uuid,
+    Instant createdAt,
+    String email,
+    String password) {
 }
