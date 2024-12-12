9adb0928ace246823efb9c96f87251c8a545a68a
ibluestarz <105120484+ibluestarz@users.noreply.github.com>
2024-12-12 19:07:52 +0100

    Update UserDto.java

--- src/main/java/fr/lernejo/todo/UserDto.java
+++ src/main/java/fr/lernejo/todo/UserDto.java
@@ -1,4 +1,53 @@
 package fr.lernejo.todo;
 
+import java.time.Instant;
+import java.util.UUID;
+
 public class UserDto {
+    private String email;
+    private String password;
+    private Instant createdAt;
+    private UUID id;
+
+    public UserDto() {
+    }
+
+    public UserDto(String email, String password, Instant createdAt, UUID id) {
+        this.email = email;
+        this.password = password;
+        this.createdAt = createdAt;
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
 }
