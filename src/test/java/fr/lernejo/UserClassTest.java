c6fb7e8336b799bfbafb503a4d71d9a346a8b167
ibluestarz <105120484+ibluestarz@users.noreply.github.com>
2024-12-12 19:11:22 +0100

    Update UserClassTest.java

--- src/test/java/fr/lernejo/UserClassTest.java
+++ src/test/java/fr/lernejo/UserClassTest.java
@@ -1,4 +1,25 @@
 package fr.lernejo;
 
-public class UserClassTest {
+import fr.lernejo.todo.UserEntity;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserClassTest {
+
+    @Test
+    void validUserEntity() {
+        Instant now = OffsetDateTime.now().toInstant();
+        UUID uuid = UUID.randomUUID();
+        UserEntity userEntity = new UserEntity(1L, uuid, now, "test@test.com", "0123456789");
+        assertNotNull(userEntity);
+        assertEquals("test@test.com", userEntity.email());
+        assertEquals("0123456789", userEntity.password());
+        assertEquals(now, userEntity.createdAt());
+        assertEquals(uuid, userEntity.uuid());
+    }
 }
